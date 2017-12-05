966279044bba89591084f1fba416d86e7b92d429
Artem <artem.veselov@7ninjas.com>
2017-12-05 11:13:48 +0100

    added functionality to email form

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,102 +1,140 @@
-$(document).ready(function() {
-  $.fn.isOnScreen = function(){
-    var win = $(window);
+$ (document).ready (function () {
+  $.fn.isOnScreen = function () {
+    var win = $ (window);
 
     var viewport = {
-      top : win.scrollTop(),
-      left : win.scrollLeft()
+      top: win.scrollTop (),
+      left: win.scrollLeft ()
     };
-    viewport.right = viewport.left + win.width();
-    viewport.bottom = viewport.top + win.height();
+    viewport.right = viewport.left + win.width ();
+    viewport.bottom = viewport.top + win.height ();
 
-    var bounds = this.offset();
-    bounds.right = bounds.left + this.outerWidth();
-    bounds.bottom = bounds.top + this.outerHeight();
+    var bounds = this.offset ();
+    bounds.right = bounds.left + this.outerWidth ();
+    bounds.bottom = bounds.top + this.outerHeight ();
 
     return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
 
   };
 
-  var heroVideo = $('#hero-video');
+  var heroVideo = $ ('#hero-video');
 
   // Run video after 2s
-  setTimeout(function(){
-    document.getElementById("hero-video").play();
+  setTimeout (function () {
+    document.getElementById ("hero-video").play ();
   }, 2000);
 
-  $('.video-wrap .play').click(function () {
-    heroVideo.prop('muted', false);
+  $ ('.video-wrap .play').click (function () {
+    heroVideo.prop ('muted', false);
   })
 
-  $('.video-wrap .mute-icon').click(function () {
-    heroVideo.prop('muted', true);
+  $ ('.video-wrap .mute-icon').click (function () {
+    heroVideo.prop ('muted', true);
   })
 
-  $(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
+  $ (document).on ('click', 'a[href^="#"]', function (event) {
+    event.preventDefault ();
 
-    $('html, body').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top
+    $ ('html, body').animate ({
+      scrollTop: $ ($.attr (this, 'href')).offset ().top
     }, 500);
   });
 
-  new WOW().init();
+  new WOW ().init ();
 
   // Email Validation/Interaction
-  function isEmail(email) {
+  function isEmail (email) {
     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    return regex.test(email);
+    return regex.test (email);
   }
 
-  var inputGroup = $('.subscribe-updates .input-group')
-  var emailBox = $('.subscribe-updates .input-group input')
+  var inputGroup = $ ('.subscribe-updates .input-group')
+  var emailBox = $ ('.subscribe-updates .input-group input')
 
-  $(".subscribe-updates .input-group button").on('click touch', function () {
-    console.log(emailBox.val());
-    if (isEmail(emailBox.val())) {
-      inputGroup.addClass('valid').removeClass('invalid');
-      setTimeout(function() {
-        inputGroup.removeClass('valid');
+  var apiUrl = "https://blockv.api-us1.com";
+  var apiString = apiUrl + "/admin/api.php";
+  var apiKey = "f2ec2ed8677320cf24e162d440927b5f993c7489e2acc5f999f385f816b8fdcd724caf87";
+
+  $ (".subscribe-updates .input-group button").on ('click touch', function () {
+    if (isEmail (emailBox.val())) {
+      var userId = Date.now();
+      var userIdProp = "p[" + userId + "]";
+      inputGroup.addClass ('valid').removeClass ('invalid');
+
+      var contact = {
+        "api_action": "contact_add",
+        "api_key": apiKey,
+        "email": emailBox.val(),
+      }
+
+      contact[userIdProp] = userId;
+
+      $.post(apiString,
+        {
+          "api_action": "contact_add",
+          "api_key": apiKey,
+          "email": emailBox.val(),
+          userIdProp: emailBox.val(),
+        }, function (response) {
+          console.log ("Response: ", response);
+        });
+
+      setTimeout (function () {
+        inputGroup.removeClass ('valid');
         emailBox.val('');
       }, 3500);
     } else {
       if (emailBox.val() == "") {
-        inputGroup.removeClass('invalid');
+        inputGroup.removeClass ('invalid');
       } else {
-        inputGroup.removeClass('valid').addClass('invalid');
+        inputGroup.removeClass ('valid').addClass ('invalid');
       }
     }
   });
 
-  emailBox.on('focus', function () {
-    inputGroup.removeClass('invalid');
+  emailBox.on ('focus', function () {
+    inputGroup.removeClass ('invalid');
   })
 
-  $(".slider-apps").slick({"dots": false, "infinite": true, "autoplay": true, "autoplaySpeed": 2000, "arrows":false});
-  $(".slider-try-it").slick({"dots": false, "infinite": true, "autoplay": true, "autoplaySpeed": 2000, "arrows":false, "fade": true, "cssEase": "linear"});
+  $ (".slider-apps").slick ({
+    "dots": false,
+    "infinite": true,
+    "autoplay": true,
+    "autoplaySpeed": 2000,
+    "arrows": false
+  });
+  $ (".slider-try-it").slick ({
+    "dots": false,
+    "infinite": true,
+    "autoplay": true,
+    "autoplaySpeed": 2000,
+    "arrows": false,
+    "fade": true,
+    "cssEase": "linear"
+  });
 
-  var roadmapList = $(".roadmap-list");
+  var roadmapList = $ (".roadmap-list");
   var roadmapItemsActive = 0;
-  var roadmapItems = roadmapList.children();
+  var roadmapItems = roadmapList.children ();
   var isVisible = false;
 
-  window.onscroll = function() {
-    if (roadmapList.isOnScreen()) {
+  window.onscroll = function () {
+    if (roadmapList.isOnScreen ()) {
       isVisible = true;
     }
   }
 
-  setInterval(function() {
-    if(isVisible) {
-      $(roadmapItems[roadmapItemsActive]).removeClass('active');
-      roadmapList.removeClass('active-' + roadmapItemsActive);
-      if(roadmapItemsActive === roadmapItems.length - 1) {
+  setInterval (function () {
+    if (isVisible) {
+      $ (roadmapItems[roadmapItemsActive]).removeClass ('active');
+      roadmapList.removeClass ('active-' + roadmapItemsActive);
+      if (roadmapItemsActive === roadmapItems.length - 1) {
         roadmapItemsActive = 0;
       } else {
         roadmapItemsActive++;
       }
-      $(roadmapItems[roadmapItemsActive]).addClass('active');
-      roadmapList.addClass('active-' + roadmapItemsActive);
+      $ (roadmapItems[roadmapItemsActive]).addClass ('active');
+      roadmapList.addClass ('active-' + roadmapItemsActive);
     }
   }, 3000);
 });

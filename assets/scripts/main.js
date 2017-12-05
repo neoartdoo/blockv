1db38b4735fcf1d7727bea456cba08bff0bc1179
Artem <artem.veselov@7ninjas.com>
2017-12-05 18:59:22 +0100

    changed team section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -150,13 +150,12 @@ $ (document).ready (function () {
 
     window.ondevicemotion = function(event) {
       var title = $('.gradient-title');
-      var posX = checkPos(parseFloat(50 - 4 * event.accelerationIncludingGravity.x).toFixed());
-      var posY = checkPos(parseFloat(65 - 2 * event.accelerationIncludingGravity.y).toFixed());
+      var posX1 = checkPos(parseFloat(60 + 4 * event.accelerationIncludingGravity.x).toFixed());
+      var posX2 = checkPos(parseFloat(40 + 4 * event.accelerationIncludingGravity.y).toFixed());
 
-      // title.css('background-image',
-      //   'linear-gradient(to left, #58421d 0%, #d0ac71 ' + (parseFloat(40 + 4 * event.accelerationIncludingGravity.x).toFixed( 2 )) +'%, #d0ac71' +
-      //   ' ' + (parseFloat(60 + 4 * event.accelerationIncludingGravity.x).toFixed( 2 )) +'%, #58421d 100%');
-      title.css('background-image', 'radial-gradient(closest-corner at '+ posX + '% ' + posY + '%, #d0ac71, #d0ac71, #58421d, #58421d)');
+      title.css('background-image',
+        'linear-gradient(to left, #58421d 0%, #d0ac71 ' + posX1 +'%, #d0ac71' + ' ' + posX2 +'%, #58421d 100%');
+
     }
   } else {
     setInterval (function () {

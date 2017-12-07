ab2a9efa79f9d08edb6f7914004e154ed6b27afe
Artem <artem.veselov@7ninjas.com>
2017-12-07 03:29:39 +0100

    updated team section content, minor fixes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,116 +1,135 @@
-$ (document).ready (function () {
-  $.fn.isOnScreen = function () {
-    var win = $ (window);
+$(document).ready(function() {
+  $.fn.isOnScreen = function() {
+    var win = $(window);
 
     var viewport = {
-      top: win.scrollTop (),
-      left: win.scrollLeft ()
+      top: win.scrollTop(),
+      left: win.scrollLeft()
     };
-    viewport.right = viewport.left + win.width ();
-    viewport.bottom = viewport.top + win.height ();
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
 
-    var bounds = this.offset ();
-    bounds.right = bounds.left + this.outerWidth ();
-    bounds.bottom = bounds.top + this.outerHeight ();
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
 
     return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
   };
 
-  function checkPos (number) {
-    if (number > 100) {
+  function checkPos(number) {
+    if(number > 100) {
       return 100;
     } else {
       return number;
     }
   }
 
-  var heroVideo = $ ('#hero-video');
+  var heroVideoWrap = $('.video-wrap');
+  var heroVideo = $('#hero-video');
+  var soundEnabled = false;
 
-  // Run video after 2s
-  setTimeout (function () {
-    document.getElementById ("hero-video").play ();
+  function toggleSound() {
+    heroVideo.prop('muted', !soundEnabled);
+
+    if(soundEnabled) {
+      heroVideoWrap.removeClass('muted')
+      heroVideoWrap.addClass('sound')
+    } else {
+      heroVideoWrap.removeClass('sound')
+      heroVideoWrap.addClass('muted')
+    }
+
+    soundEnabled = !soundEnabled;
+  }
+
+  // Video script
+  setTimeout(function() {
+    document.getElementById("hero-video").play();
   }, 2000);
 
-  $ ('.video-wrap .play').click (function () {
-    heroVideo.prop ('muted', false);
+  $('.video-wrap .play').click(function() {
+    toggleSound();
   })
 
-  $ ('.video-wrap .mute-icon').click (function () {
-    heroVideo.prop ('muted', true);
+  $('.status-icon').click(function() {
+    toggleSound();
   })
 
-  $ (document).on ('click', 'a[href^="#"]', function (event) {
-    event.preventDefault ();
 
-    $ ('html, body').animate ({
-      scrollTop: $ ($.attr (this, 'href')).offset ().top
+  //anchor link
+  $(document).on('click', 'a[href^="#"]', function(event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
   });
 
-  new WOW ().init ();
+  //WOW init
+  new WOW().init();
 
   // Email Validation/Interaction
-  function isEmail (email) {
+  function isEmail(email) {
     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    return regex.test (email);
+    return regex.test(email);
   }
 
-  var contactForm = $ ('.subscribe-updates');
-  var contactFormSubmitButton = $ (".subscribe-updates .input-group button");
-  var inputGroup = $ ('.subscribe-updates .input-group')
-  var emailBox = $ ('.subscribe-updates .input-group input')
+  var contactForm = $('.subscribe-updates');
+  var contactFormSubmitButton = $(".subscribe-updates .input-group button");
+  var inputGroup = $('.subscribe-updates .input-group')
+  var emailBox = $('.subscribe-updates .input-group input')
 
-  contactForm.submit (function (event) {
-    event.preventDefault ();
-    contactFormSubmitButton.click ();
+  contactForm.submit(function(event) {
+    event.preventDefault();
+    contactFormSubmitButton.click();
   });
 
   var apiUrl = "https://blockv.api-us1.com";
   var apiString = apiUrl + "/admin/api.php";
   var apiKey = "f2ec2ed8677320cf24e162d440927b5f993c7489e2acc5f999f385f816b8fdcd724caf87";
 
-  contactFormSubmitButton.on ('click touch', function () {
-    if (isEmail (emailBox.val ())) {
-      var userId = Date.now ();
+  contactFormSubmitButton.on('click touch', function() {
+    if(isEmail(emailBox.val())) {
+      var userId = Date.now();
       var userIdProp = "p[" + userId + "]";
-      inputGroup.addClass ('valid').removeClass ('invalid');
+      inputGroup.addClass('valid').removeClass('invalid');
 
       var contact = {
         "api_action": "contact_add",
         "api_key": apiKey,
-        "email": emailBox.val (),
+        "email": emailBox.val(),
       }
 
       contact[userIdProp] = userId;
 
-      $.post (apiString,
+      $.post(apiString,
         {
           "api_action": "contact_add",
           "api_key": apiKey,
-          "email": emailBox.val (),
+          "email": emailBox.val(),
           userIdProp: userId,
-        }, function (response) {
-          console.log ("Response: ", response);
+        }, function(response) {
+          console.log("Response: ", response);
         });
 
-      setTimeout (function () {
-        inputGroup.removeClass ('valid');
-        emailBox.val ('');
+      setTimeout(function() {
+        inputGroup.removeClass('valid');
+        emailBox.val('');
       }, 3500);
     } else {
-      if (emailBox.val () == "") {
-        inputGroup.removeClass ('invalid');
+      if(emailBox.val() == "") {
+        inputGroup.removeClass('invalid');
       } else {
-        inputGroup.removeClass ('valid').addClass ('invalid');
+        inputGroup.removeClass('valid').addClass('invalid');
       }
     }
   });
 
-  emailBox.on ('focus', function () {
-    inputGroup.removeClass ('invalid');
+  emailBox.on('focus', function() {
+    inputGroup.removeClass('invalid');
   })
 
-  $ (".slider-apps").slick ({
+  $(".slider-apps").slick({
     "dots": false,
     "infinite": true,
     "autoplay": true,
@@ -118,9 +137,9 @@ $ (document).ready (function () {
     "arrows": false
   });
 
-  var tryItSlider = $ (".slider-try-it");
+  var tryItSlider = $(".slider-try-it");
 
-  tryItSlider.slick ({
+  tryItSlider.slick({
     "dots": false,
     "infinite": true,
     "false": true,
@@ -139,14 +158,14 @@ $ (document).ready (function () {
   var emailFormIsVisible = false;
   var emailFormEnabled = false;
 
-  function enablePlaceholder () {
+  function enablePlaceholder() {
     var i = 0;
     var txt = 'Enter your e-mail address';
     var speed = 100;
     var placeholderText = '';
 
     function typeWriter() {
-      if (i < txt.length) {
+      if(i < txt.length) {
         placeholderText += txt.charAt(i);
         emailForm.attr('placeholder', placeholderText);
         i++;
@@ -157,15 +176,18 @@ $ (document).ready (function () {
     typeWriter();
   }
 
-  window.onscroll = function () {
+  window.onscroll = function() {
     // if (roadmapList.isOnScreen ()) {
     //   roadmapIsVisible = true;
     // }
 
-    if (!emailFormEnabled) {
+    if(!emailFormEnabled) {
       emailFormEnabled = true;
-      setTimeout(function () {
-        if (emailForm.isOnScreen ()) {enablePlaceholder()};
+      setTimeout(function() {
+        if(emailForm.isOnScreen()) {
+          enablePlaceholder
+        }
+        ;
       }, 2000);
     }
   }
@@ -174,15 +196,15 @@ $ (document).ready (function () {
   //   roadmapList.removeClass ('active-' + roadmapItemsActive);
   // }
 
-  if (window.matchMedia ('(max-width: 767px)').matches) {
+  if(window.matchMedia('(max-width: 767px)').matches) {
     // roadmapItems.click (function () {
     //   roadmapItems.removeClass ('active');
     //   $ (this).addClass ('active');
     // })
 
-    window.ondevicemotion = function (event) {
-      var title = $ ('.gradient-title');
-      title.css ('background-image', 'linear-gradient(to left, #58421d 0%, #d0ac71 ' + (parseFloat (40 + 4 * event.accelerationIncludingGravity.x).toFixed (2)) + '%, #d0ac71 ' + (parseFloat (60 + 4 * event.accelerationIncludingGravity.x).toFixed (2)) + '%, #58421d 100%')
+    window.ondevicemotion = function(event) {
+      var title = $('.gradient-title');
+      title.css('background-image', 'linear-gradient(to left, #58421d 0%, #d0ac71 ' + (parseFloat(40 + 4 * event.accelerationIncludingGravity.x).toFixed(2)) + '%, #d0ac71 ' + (parseFloat(60 + 4 * event.accelerationIncludingGravity.x).toFixed(2)) + '%, #58421d 100%')
     }
   } else {
     // setInterval (function () {
@@ -203,48 +225,35 @@ $ (document).ready (function () {
     //   $ (this).addClass ('active');
     // });
   }
-  
+
   // Gift button
-  var giftButton = $('#gift-button');
-  var giftButtonText = $('#gift-button .text');
+  var giftBlockIsVisible = false;
   var giftStep = 0;
   var giftStepsList = $('.steps-list li');
-  var giftProgressbarWrap = $('.gift-progress');
-  var giftProgressbar = $('.gift-progress .progress-bar');
 
-  giftButton.click(giftButtonEvent);
+  window.onscroll = function() {
+    if($('.steps-list').isOnScreen() && !giftBlockIsVisible) {
+      giftBlockIsVisible = true;
 
-  function giftButtonEvent () {
+      setInterval (function () {
+        giftButtonEvent();
+      }, 3000);
+    }
+  }
+
+  function giftButtonEvent() {
     switch(giftStep) {
-      case 0:
-        $('.present-image-wrap').removeClass('animated');
-        giftButtonText.text('Next Step');
-        giftStepsList.removeClass('active');
-        giftStepsList.eq( giftStep ).addClass('active');
-        tryItSlider.slick('slickGoTo', giftStep + 1);
-        giftProgressbar.css('width', '18%');
-        giftProgressbarWrap.css('opacity', 1);
-        break;
-      case 5:
-        giftButtonText.text('Start again');
-        giftStepsList.removeClass('active');
-        giftStepsList.eq( giftStep ).addClass('active');
-        tryItSlider.slick('slickGoTo', giftStep + 1);
-        giftProgressbar.css('width', '100%');
-        break;
       case 6:
-        giftButtonText.text('Next Step');
         giftStepsList.removeClass('active');
         giftStepsList.eq(0).addClass('active');
-        giftProgressbar.css('width', '0');
         giftStep = 0;
+        tryItSlider.slick('slickGoTo', 0);
         break;
       default:
         giftStepsList.removeClass('active');
-        giftStepsList.eq( giftStep ).addClass('active');
-        if( giftStep < 6) {
+        giftStepsList.eq(giftStep).addClass('active');
+        if(giftStep < 6) {
           tryItSlider.slick('slickGoTo', giftStep + 1);
-          giftProgressbar.css('width', 100 / 6 * (giftStep + 1) + '%');
         }
     }
     giftStep++;
